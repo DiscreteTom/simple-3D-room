b9b1c3428df73de4b5306b5b5bbbafb06a94a7c9
DiscreteTom <discrete_tom@outlook.com>
2019-05-30 18:16:58 +0800

    finish setupRC, add light tracking

--- simple-3D-room/simple-3D-room.cpp
+++ simple-3D-room/simple-3D-room.cpp
@@ -6,6 +6,11 @@
 #pragma comment(lib, "freeglut.lib")
 #endif
 
+// Light values and coordinates
+GLfloat ambientLight[] = {1.0f, 1.0f, 1.0f, 1.0f};
+GLfloat diffuseLight[] = {1.0f, 1.0f, 1.0f, 1.0f};
+GLfloat specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
+GLfloat specref[] = {1.0f, 1.0f, 1.0f, 1.0f};
 // display callback function
 void render()
 {
@@ -21,6 +26,34 @@ void render()
 void setupRC()
 {
 	glClearColor(1, 1, 1, 1); // white background
+
+	// Enable Depth Testing
+	glEnable(GL_DEPTH_TEST);
+
+	// Enable lighting
+	glEnable(GL_LIGHTING);
+
+	// Setup and enable light 0
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+	glEnable(GL_LIGHT0);
+
+	glShadeModel(GL_SMOOTH);
+
+	// Enable color tracking
+	glEnable(GL_COLOR_MATERIAL);
+
+	// Set Material properties to follow glColor values
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+	// All materials hereafter have full specular reflectivity
+	// with a high shine
+	glMaterialfv(GL_FRONT, GL_SPECULAR, specref);
+	glMateriali(GL_FRONT, GL_SHININESS, 128);
+
+	// Set drawing color to blue
+	glColor3ub(0, 0, 255);
 }
 
 // keyboard callback function(for ascii chars)
